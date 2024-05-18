a40b5e0cac8418f7a9ba084ef95f71d85c8b382e
hancandice <jeeyoung.han82@gmail.com>
2024-05-18 16:08:59 +0200

    Refactor: Simplified Counter contract initialization and increment method

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,21 +2,10 @@
 pragma solidity ^0.8.0;
 
 contract Counter {
-   uint private count;
-
-   constructor() {
-       count = 0;
-   }
-
-   event CountIncremented(uint newCount);
-
-   function getCount() public view returns (uint) {
-       return count;
-   }
+   uint public count = 0;
 
    function incrementCount() public {
        count++;
-       emit CountIncremented(count);
    }
 }
 
